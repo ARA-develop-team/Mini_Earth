050efe16073ffc19365477abdc3122b5b28bf68d
Anton <over.uran@gmail.com>
2020-12-24 11:53:04 +0200

    class_block_draw

--- class_block.py
+++ class_block.py
@@ -1,1 +1,22 @@
-print("h")
+import pygame
+
+
+class CBlock:
+    def __init__(self, x, y, size, height_ground, height_water, temp_surface, temp_air, clouds):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.height_ground = height_ground
+        self.height_water = height_water
+        self.temp_surface = temp_surface
+        self.temp_air = temp_air
+        self.cloud_concentration = clouds
+        self.vegetation = []
+        self.isDay = True
+
+    def draw(self, screen):
+        color = (139, 69, 19)
+        if self.height_water > 100:
+            color = (30, 144, 255)
+        pygame.draw.rect(screen, color, (self.x, self.y, self.size, self.size))
+
