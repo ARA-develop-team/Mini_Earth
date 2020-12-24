ebb640b627b17b93d88ce83bd5db6dd13f728efe
Anton <over.uran@gmail.com>
2020-12-24 12:54:42 +0200

    generation_of_world

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from class_block import CBlock
 
 window_x = 500
@@ -12,10 +13,22 @@ height_cam = window_y
 zoom = 1
 
 pygame.init()
+num_horizontal = 100
+num_vertical = 100
+block_size = 0
 block_list = []
-block_list.append(CBlock(100, 100, 10, 101, 101, 10, 10, 10))
-block_list.append(CBlock(100, 110, 10, 101, 10, 10, 10, 10))
-block_list.append(CBlock(100, 120, 10, 101, 101, 10, 10, 10))
+new_x = 0
+new_y = 0
+for column in range(num_vertical):
+    for new_block in range(num_horizontal):
+        block_list.append(CBlock(new_x, new_y, 10, 101, random.randint(99, 101), 10, 10, 10))
+        new_x += block_size
+    new_y += block_size
+    new_x = 0
+# block_list.append(CBlock(100, 100, 10, 101, 101, 10, 10, 10))
+# block_list.append(CBlock(100, 110, 10, 101, 10, 10, 10, 10))
+# block_list.append(CBlock(100, 120, 10, 101, 101, 10, 10, 10))
+
 run = True
 while run:
     for e in pygame.event.get():
@@ -24,13 +37,13 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
-        x_cam += 0.5
+        x_cam += 1
     if keys[pygame.K_a]:
-        x_cam -= 0.5
+        x_cam -= 1
     if keys[pygame.K_w]:
-        y_cam -= 0.5
+        y_cam -= 1
     if keys[pygame.K_s]:
-        y_cam += 0.5
+        y_cam += 1
 
     window.fill((47, 79, 79))
     for block in block_list:
