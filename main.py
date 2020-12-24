914bb5b5580dd0bf8a9b24d9952119642c44da3b
IcePhoenix09 <63466963+IcePhoenix09@users.noreply.github.com>
2020-12-24 14:30:55 +0200

    zoom

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ zoom = 1
 pygame.init()
 num_horizontal = 100
 num_vertical = 100
-block_size = 0
+block_size = 10
 block_list = []
 new_x = 0
 new_y = 0
@@ -44,10 +44,24 @@ while run:
         y_cam -= 1
     if keys[pygame.K_s]:
         y_cam += 1
+    if keys[pygame.K_r]:
+        length_cam += 20
+        height_cam += 20
+        x_cam -= 10
+        y_cam -= 10
+    if keys[pygame.K_f]:
+        if length_cam > 1 and height_cam > 1:
+            length_cam -= 20
+            height_cam -= 20
+            x_cam += 10
+            y_cam += 10
 
     window.fill((47, 79, 79))
     for block in block_list:
         if block.x + block.size > x_cam and block.x < x_cam + length_cam and block.y + block.size > y_cam and block.y < y_cam + height_cam:
-            block.draw(window, block.x - x_cam, block.y - y_cam, block.size)
+            x = (block.x - x_cam) * (window_x / length_cam)
+            y = (block.y - y_cam) * (window_y / height_cam)
+            size = window_x / (length_cam / block.size)
+            block.draw(window, x, y, size)
     pygame.display.update()
 pygame.quit()
